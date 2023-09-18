63fc8293e1dc74730681b4c52d6621496403527f
Lucas Ramirez <slramirez2002@gmail.com>
2023-09-18 12:05:05 -0400

    Created entry function and exclusive main function

---
+++ patient_db.py
@@ -0,0 +1,15 @@
+db = []
+def create_db_entry(patient_name, mrn, age):
+   new_patient = [patient_name, mrn, age, []]
+   return new_patient
+
+def main():
+    db.append(create_db_entry("Ann Ables", 101, 35))
+    dp.append(create_db_entry("Bob Boyles", 102, 64))
+    dp.append(create_db_entry("Chris Chou", 103, 23))
+    print(db)
+    
+if __name__ == "__main__":
+    main()
+
+    
