727afdc6cecafe238a6571a4fcc1bff197e2313b
Lucas Ramirez <slramirez2002@gmail.com>
2023-09-18 12:10:45 -0400

    Created print function

--- patient_db.py
+++ patient_db.py
@@ -9,6 +9,10 @@ def create_db_entry(patient_name, mrn, age):
    new_patient = [patient_name, mrn, age, []]
    return new_patient
 
+def printdb():
+    for p in db:
+        print('Name: {}, MRN: {}, Age: {}'.format(p[0], p[1], p[2]))
+
 def main():
     db.append(create_db_entry("Ann Ables", 101, 35))
     dp.append(create_db_entry("Bob Boyles", 102, 64))
