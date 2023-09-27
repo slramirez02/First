e68e43329f0aff68fc9dc618b5589d5704eaf482
Lucas Ramirez <slramirez2002@gmail.com>
2023-09-27 12:29:10 -0400

    converted patient entries to dictionaries, added minor_or_adult function

--- patient_db.py
+++ patient_db.py
@@ -5,25 +5,46 @@
 """
 
 db = []
-def create_db_entry(patient_name, mrn, age):
-   new_patient = [patient_name, mrn, age, []]
+def create_db_entry(firstName, lastName, mrn, age):
+   new_patient = {
+   "First Name" : firstName, 
+   "Last Name" : lastName, 
+   "MRN" : mrn,
+   "Age" : age,
+   "Tests": []}
    return new_patient
 
 def printdb():
+    for patient in db:
+        print("Name: {}, MRN: {}, Age: {}"
+            .format(get_fullname(patient),
+                    patient["MRN"],
+                    patient["Age"]))
+    '''
     rooms = ["A3", "B1", "C2"]
     for i, p in enumerate(db):
         print("Name: {}, MRN: {}, Age: {}, Room: {}".format(p[0], p[1], p[2], rooms[i]))
     for room, p, in zip(rooms, db):
         print("Name: {}, MRN: {}, Age: {}, Room: {}".format(p[0], p[1], p[2], room))
+    '''
 
+def get_fullname(patient):
+    return "{} {}".format(patient["First Name"],
+                          patient["Last Name"])
 def main():
-    db.append(create_db_entry("Ann Ables", 101, 35))
-    db.append(create_db_entry("Bob Boyles", 102, 64))
-    db.append(create_db_entry("Chris Chou", 103, 23))
-    print(db)
+    db.append(create_db_entry("Ann", "Ables", 101, 35))
+    db.append(create_db_entry("Bob", "Boyles", 102, 64))
+    db.append(create_db_entry("Chris", "Chou", 103, 23))
+    #print(db)
 
 def find_patient(mrn):
-    answer = []
+    answer = False
+    for patient in db:
+        if patient["MRN"] == mrn:
+            answer = patient
+            break
+    return answer
+    '''
     for i, p in enumerate(db):
         if p[1] == mrn:
             answer = p
@@ -33,17 +54,27 @@ def find_patient(mrn):
         ind = -1 
     else:
         return ind, answer
-        
-def add_testresult(mrn, test_name, test_result):
-    patient_ind = find_patient(mrn)[0]
-    db[patient_ind][3].append((test_name, test_result))
-    print(db)
+    '''
+def add_test_to_patient(mrn, test_name, test_result):
+    patient = find_patient(mrn)
+    patient["Tests"].append((test_name, test_result))
+    
+def minor_or_adult(patient):
+    age = patient["Age"]
+    answer = ""
+    if age >= 18:
+        answer = "Adult"
+    else:
+        answer = "Minor"
+    return answer
+            
+
     
     
 if __name__ == "__main__":
     main()
+    #find_patient(102)
+    add_test_to_patient(102, "weight", "125")
     printdb()
-    find_patient(102)
-    add_testresult(102, "weight", "125")
 
     
