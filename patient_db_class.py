fae56b407de3b6bb47271dc4ed61ac696e1adf27
Lucas Ramirez <slramirez2002@gmail.com>
2023-10-02 11:51:15 -0400

    Add parameters to Class methods

--- patient_db_class.py
+++ patient_db_class.py
@@ -1,16 +1,16 @@
 class Patient:
 
-    def __init__(self):
+    def __init__(self, fn, ln, mrn, age):
         '''
         initialization func in python has this special name
         self.variable refers to variables that will be local to class instance itself
         '''
-        self.first_name = ""    
-        self.last_name = ""
-        self.mrn = None
-        self.age = None
+        self.first_name = fn    
+        self.last_name = ln
+        self.mrn = mrn
+        self.age = age
         self.tests = []
-        z = "hello"
+        self.z = "hello"
     
     def print(self):
         out_string = "Name: {}".format(self.full_name())
@@ -19,3 +19,7 @@ class Patient:
     
     def full_name(self):
         return "{} {}".format(self.first_name, self.last_name)
+    
+    def change_age(new_age):
+        self.age = new_age
+        return None
