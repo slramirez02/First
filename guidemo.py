d9823ee44dc187117793bd1419b06742abd6324e
slramirez02 <slramirez2002@gmail.com>
2023-11-13 12:45:43 -0500

    Classwork with GUI buttons

---
+++ guidemo.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+def main_window():
+
+    def swap_colors():
+        current_color = label.cget("foreground")
+        if current_color.string == "red":
+            label.configure(foreground="black")
+        else:
+            label.configure(foreground="red")
+        root.after(1000, swap_colors)
+
+    def button_cmd():
+        messagebox.askyesno("Verification of Action",
+                            "Are you sure you want blinking letters?")
+        label.configure(foreground="red")
+        root.after(1000, swap_colors)
+
+
+    root = tk.Tk()
+    root.title("GUI Demo")
+
+    label = ttk.Label(root, text="Welcome", font=("Arial", 24))
+    label.grid(column=0, row=0)
+
+    button = ttk.Button(root, text="Ok", command=button_cmd)
+    button.grid(column=0, row=1)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main_window()
